8cf754c37ba90fb195eedc9e7a9899b69067de9d
Hubert <golab.mlody@gmail.com>
2022-04-08 21:27:21 +0200

    Set Swagger

--- API/Program.cs
+++ API/Program.cs
@@ -8,10 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationLayer();
 builder.Services.AddPersistanceLayer(builder.Configuration);
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwaggerUI();
+app.UseSwagger(x => x.SerializeAsV2 = true);
 // Configure the HTTP request pipeline.
 
 app.Run();
