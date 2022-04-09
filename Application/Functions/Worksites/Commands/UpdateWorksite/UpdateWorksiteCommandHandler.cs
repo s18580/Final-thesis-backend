2bdab3c6cfce4f668077b1374bc19b5526e56e78
Hubert <golab.mlody@gmail.com>
2022-04-09 08:23:09 +0200

    Delete unused mapper

--- Application/Functions/Worksites/Commands/UpdateWorksite/UpdateWorksiteCommandHandler.cs
+++ Application/Functions/Worksites/Commands/UpdateWorksite/UpdateWorksiteCommandHandler.cs
@@ -1,6 +1,5 @@
 ﻿using Application.Services;
 using AutoMapper;
-using Domain.Models.DictionaryModels;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,12 +8,10 @@ namespace Application.Functions.Worksites.Commands.UpdateWorksite
     public class UpdateWorksiteCommandHandler : IRequestHandler<UpdateWorksiteCommand, UpdateWorksiteResponse>
     {
         private readonly IApplicationContext _context;
-        private readonly IMapper _mapper;
 
-        public UpdateWorksiteCommandHandler(IApplicationContext context, IMapper mapper)
+        public UpdateWorksiteCommandHandler(IApplicationContext context)
         {
             _context = context;
-            _mapper = mapper;
         }
 
         public async Task<UpdateWorksiteResponse> Handle(UpdateWorksiteCommand request, CancellationToken cancellationToken)
@@ -24,10 +21,9 @@ namespace Application.Functions.Worksites.Commands.UpdateWorksite
 
             if (!validatorResult.IsValid) return new UpdateWorksiteResponse(validatorResult, Responses.ResponseStatus.ValidationError);
 
-            //var updatedWorksite = _mapper.Map<Worksite>(request);
             var selectedWorksite = await _context.Worksites.Where(p => p.IdWorksite == request.IdWorksite).SingleAsync();
 
-            if (selectedWorksite.Name != request.Name) { selectedWorksite.Name = request.Name }
+            if (selectedWorksite.Name != request.Name) { selectedWorksite.Name = request.Name; }
             
             await _context.SaveChangesAsync();
 
