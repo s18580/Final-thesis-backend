ecc45a6d05b27837247fc7917fb9c2ebe688affa
Hubert <golab.mlody@gmail.com>
2022-04-10 17:39:33 +0200

    Add GetRoleAssignmentQuery

---
+++ Application/Functions/RoleAssignment/Queries/GetRoleAssignmentQuery/GetRoleAssignmentQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.RoleAssignment.Queries.GetRoleAssignmentQuery
+{
+    public class GetRoleAssignmentQueryHandler : IRequestHandler<GetRoleAssignmentQuery, Domain.Models.RoleAssignment>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetRoleAssignmentQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Models.RoleAssignment> Handle(GetRoleAssignmentQuery request, CancellationToken cancellationToken)
+        {
+            var roleAssignment = await _context.RoleAssignments
+                                               .Where(p => p.IdRole == request.IdRole)
+                                               .Where(p => p.IdWorker == request.IdWorker)
+                                               .SingleOrDefaultAsync();
+
+            return roleAssignment;
+        }
+    }
+}
