da02d44bef1f62154c770bf702e8f3e117211235
Hubert <golab.mlody@gmail.com>
2022-04-13 20:55:47 +0200

    Add OrderItemType queries

---
+++ Application/Functions/OrderItemType/Queries/GetOrderItemTypeQuery/GetOrderItemTypeQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Functions.OrderItemType.Queries.GetOrderItemTypeQuery
+{
+    public class GetOrderItemTypeQuery : IRequest<Domain.Models.DictionaryModels.OrderItemType>
+    {
+        public int IdOrderItemType { get; set; }
+    }
+}
---
+++ Application/Functions/OrderItemType/Queries/GetOrderItemTypeQuery/GetOrderItemTypeQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.OrderItemType.Queries.GetOrderItemTypeQuery
+{
+    public class GetOrderItemTypeQueryHandler : IRequestHandler<GetOrderItemTypeQuery, Domain.Models.DictionaryModels.OrderItemType>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetOrderItemTypeQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Models.DictionaryModels.OrderItemType> Handle(GetOrderItemTypeQuery request, CancellationToken cancellationToken)
+        {
+            var orderItemType = await _context.OrderItemTypes
+                                               .Where(p => p.IdOrderItemType == request.IdOrderItemType)
+                                               .SingleOrDefaultAsync();
+
+            return orderItemType;
+        }
+    }
+}
