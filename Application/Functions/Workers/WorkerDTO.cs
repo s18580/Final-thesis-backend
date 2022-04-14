b73cd9e6e047c467320bd64afe0a06b88d5442e0
Hubert <golab.mlody@gmail.com>
2022-04-14 19:57:44 +0200

    WorkerDTO compilation fix

--- Application/Functions/Workers/WorkerDTO.cs
+++ Application/Functions/Workers/WorkerDTO.cs
@@ -14,7 +14,7 @@ namespace Application.Functions.Workers
         public int? IdWorksite { get; set; }
 
         public Worksite Worksite { get; set; }
-        public ICollection<RoleAssignment> RoleAssignments { get; set; }
+        public ICollection<Domain.Models.RoleAssignment> RoleAssignments { get; set; }
         public ICollection<Customer> Customers { get; set; }
         public ICollection<Valuation> Valuations { get; set; }
         public ICollection<Assignment> Assignments { get; set; }
