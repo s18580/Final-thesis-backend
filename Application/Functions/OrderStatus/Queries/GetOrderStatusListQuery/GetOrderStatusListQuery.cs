527f7692113eed543e31580f8b392151fd254f4c
Hubert <golab.mlody@gmail.com>
2022-04-14 18:33:51 +0200

    Add OrderStatus queries

---
+++ Application/Functions/OrderStatus/Queries/GetOrderStatusListQuery/GetOrderStatusListQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Functions.OrderStatus.Queries.GetOrderStatusListQuery
+{
+    public class GetOrderStatusListQuery : IRequest<List<Domain.Models.DictionaryModels.OrderStatus>>
+    {
+    }
+}
---
+++ Application/Functions/OrderStatus/Queries/GetOrderStatusListQuery/GetOrderStatusListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.OrderStatus.Queries.GetOrderStatusListQuery
+{
+    public class GetOrderStatusListQueryHandler : IRequestHandler<GetOrderStatusListQuery, List<Domain.Models.DictionaryModels.OrderStatus>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetOrderStatusListQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<List<Domain.Models.DictionaryModels.OrderStatus>> Handle(GetOrderStatusListQuery request, CancellationToken cancellationToken)
+        {
+            var orderStatuses = await _context.OrderStatuses
+                                              .ToListAsync();
+
+            return orderStatuses;
+        }
+    }
+}
