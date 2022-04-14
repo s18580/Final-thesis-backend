ccbc7446f8f4f0590566c1eabebb68870665df0c
Hubert <golab.mlody@gmail.com>
2022-04-14 19:31:43 +0200

    Add FileStatus queries

---
+++ Application/Functions/FileStatus/Queries/GetFileStatusListQuery/GetFileStatusListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.FileStatus.Queries.GetFileStatusListQuery
+{
+    public class GetFileStatusListQueryHandler : IRequestHandler<GetFileStatusListQuery, List<Domain.Models.DictionaryModels.FileStatus>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetFileStatusListQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<List<Domain.Models.DictionaryModels.FileStatus>> Handle(GetFileStatusListQuery request, CancellationToken cancellationToken)
+        {
+            var fileStatuses = await _context.FileStatuses
+                                             .ToListAsync();
+
+            return fileStatuses;
+        }
+    }
+}
