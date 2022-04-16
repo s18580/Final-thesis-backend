a8d8536f5a90c1daa42f3f554ce431d9afd061df
Hubert <golab.mlody@gmail.com>
2022-04-16 13:42:58 +0200

    Add small name change in Representative model

--- Domain/Models/Representative.cs
+++ Domain/Models/Representative.cs
@@ -5,7 +5,7 @@
         public int IdRepresentative { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public string PhonerNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string EmailAddress { get; set; }
         public int IdOwner { get; set; }
 
