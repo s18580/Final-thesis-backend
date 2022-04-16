ce4b91b07534066ada974778cdb1333e0adf56d2
Hubert <golab.mlody@gmail.com>
2022-04-16 12:09:40 +0200

    Add Supplier queries

---
+++ Application/Functions/Supplier/Queries/GetSupplierListQuery/GetSupplierListQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Functions.Supplier.Queries.GetSupplierListQuery
+{
+    public class GetSupplierListQuery : IRequest<List<Domain.Models.Supplier>>
+    {
+    }
+}
---
+++ Application/Functions/Supplier/Queries/GetSupplierListQuery/GetSupplierListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.Supplier.Queries.GetSupplierListQuery
+{
+    public class GetSupplierListQueryHandler : IRequestHandler<GetSupplierListQuery, List<Domain.Models.Supplier>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetSupplierListQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Domain.Models.Supplier>> Handle(GetSupplierListQuery request, CancellationToken cancellationToken)
+        {
+            var suppliers = await _context.Suppliers
+                                          .ToListAsync();
+
+            return suppliers;
+        }
+    }
+}
