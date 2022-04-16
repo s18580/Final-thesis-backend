eec5a28c242f71adc844153ac57d591410bba8f4
Hubert <golab.mlody@gmail.com>
2022-04-16 15:46:45 +0200

    Add Order queries

---
+++ Application/Functions/Order/Queries/GetOrderListQuery/GetOrderListQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Functions.Order.Queries.GetOrderListQuery
+{
+    public class GetOrderListQuery : IRequest<List<Domain.Models.Order>>
+    {
+    }
+}
---
+++ Application/Functions/Order/Queries/GetOrderListQuery/GetOrderListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.Order.Queries.GetOrderListQuery
+{
+    public class GetOrderListQueryHandler : IRequestHandler<GetOrderListQuery, List<Domain.Models.Order>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetOrderListQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<List<Domain.Models.Order>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _context.Orders
+                                       .ToListAsync();
+
+            return orders;
+        }
+    }
+}
