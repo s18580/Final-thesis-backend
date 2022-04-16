43205f250ea894626b6cca696ba5843a34b97f33
Hubert <golab.mlody@gmail.com>
2022-04-16 12:36:30 +0200

    Add Representatives queries

---
+++ Application/Functions/Representative/Queries/GetRepresentativeListQuery/GetRepresentativeListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.Representative.Queries.GetRepresentativeListQuery
+{
+    public class GetRepresentativeListQueryHandler : IRequestHandler<GetRepresentativeListQuery, List<Domain.Models.Representative>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetRepresentativeListQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Domain.Models.Representative>> Handle(GetRepresentativeListQuery request, CancellationToken cancellationToken)
+        {
+            var representatives = await _context.Representatives
+                                                .ToListAsync();
+
+            return representatives;
+        }
+    }
+}
---
+++ Application/Functions/Representative/Queries/GetRepresentativeQuery/GetRepresentativeQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.Representative.Queries.GetRepresentativeQuery
+{
+    public class GetRepresentativeQueryHandler : IRequestHandler<GetRepresentativeQuery, Domain.Models.Representative>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetRepresentativeQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Models.Representative> Handle(GetRepresentativeQuery request, CancellationToken cancellationToken)
+        {
+            var representative = await _context.Representatives
+                                               .Where(p => p.IdRepresentative == request.IdRepresentative)
+                                               .SingleOrDefaultAsync();
+
+            return representative;
+        }
+    }
+}
