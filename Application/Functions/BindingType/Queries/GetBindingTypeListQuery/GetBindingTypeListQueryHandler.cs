4f88ed92f6842fc4b32f259966c368ac5a97e82c
Hubert <golab.mlody@gmail.com>
2022-04-21 12:00:22 +0200

    Add binding type queries

---
+++ Application/Functions/BindingType/Queries/GetBindingTypeListQuery/GetBindingTypeListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.BindingType.Queries.GetBindingTypeListQuery
+{
+    public class GetBindingTypeListQueryHandler : IRequestHandler<GetBindingTypeListQuery, List<Domain.Models.DictionaryModels.BindingType>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetBindingTypeListQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<List<Domain.Models.DictionaryModels.BindingType>> Handle(GetBindingTypeListQuery request, CancellationToken cancellationToken)
+        {
+            var bindingTypes = await _context.BindingTypes
+                                             .ToListAsync();
+
+            return bindingTypes;
+        }
+    }
+}
---
+++ Application/Functions/BindingType/Queries/GetBindingTypeQuery/GetBindingTypeQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.BindingType.Queries.GetBindingTypeQuery
+{
+    public class GetBindingTypeQueryHandler : IRequestHandler<GetBindingTypeQuery, Domain.Models.DictionaryModels.BindingType>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetBindingTypeQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<Domain.Models.DictionaryModels.BindingType> Handle(GetBindingTypeQuery request, CancellationToken cancellationToken)
+        {
+            var bindingType = await _context.BindingTypes
+                                            .Where(p => p.IdBindingType == request.IdBindingType)
+                                            .SingleOrDefaultAsync();
+
+            return bindingType;
+        }
+    }
+}
