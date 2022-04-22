b095981063c323f8cc0f440f7ed1aa24aab2f1ee
Hubert <golab.mlody@gmail.com>
2022-04-22 10:50:28 +0200

    Add service name queries

---
+++ Application/Functions/ServiceName/Queries/GetServiceNameQuery/GetServiceNameQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Functions.ServiceName.Queries.GetServiceNameQuery
+{
+    public class GetServiceNameQuery : IRequest<Domain.Models.DictionaryModels.ServiceName>
+    {
+        public int IdServiceName { get; set; }
+    }
+}
---
+++ Application/Functions/ServiceName/Queries/GetServiceNameQuery/GetServiceNameQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.ServiceName.Queries.GetServiceNameQuery
+{
+    public class GetServiceNameQueryHandler : IRequestHandler<GetServiceNameQuery, Domain.Models.DictionaryModels.ServiceName>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetServiceNameQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Models.DictionaryModels.ServiceName> Handle(GetServiceNameQuery request, CancellationToken cancellationToken)
+        {
+            var serviceName = await _context.ServiceNames
+                                            .Where(p => p.IdServiceName == request.IdServiceName)
+                                            .SingleOrDefaultAsync();
+
+            return serviceName;
+        }
+    }
+}
