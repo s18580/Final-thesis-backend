7276ec80c994a5977a752d4ac8a991ac2a4b325a
Hubert <golab.mlody@gmail.com>
2022-04-23 14:50:28 +0200

    Add color queries

---
+++ Application/Functions/Color/Queries/GetColorListByOrderItemQuery/GetColorListByOrderItemQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.Color.Queries.GetColorListByOrderItemQuery
+{
+    public class GetColorListByOrderItemQueryHandler : IRequestHandler<GetColorListByOrderItemQuery, GetColorListByOrderItemResponse>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetColorListByOrderItemQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetColorListByOrderItemResponse> Handle(GetColorListByOrderItemQuery request, CancellationToken cancellationToken)
+        {
+            var validator = new GetColorListByOrderItemValidator(_context);
+            var validatorResult = await validator.ValidateAsync(request);
+
+            if (!validatorResult.IsValid) return new GetColorListByOrderItemResponse(validatorResult, Responses.ResponseStatus.ValidationError);
+
+            var colors = await _context.Colors
+                                       .Where(p => p.IdLink == request.IdOrderItem)
+                                       .ToListAsync();
+
+            return new GetColorListByOrderItemResponse(colors);
+        }
+    }
+}
---
+++ Application/Functions/Color/Queries/GetColorQuery/GetColorQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.Color.Queries.GetColorQuery
+{
+    public class GetColorQueryHandler : IRequestHandler<GetColorQuery, Domain.Models.Color>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetColorQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Models.Color> Handle(GetColorQuery request, CancellationToken cancellationToken)
+        {
+            var color = await _context.Colors
+                                      .Where(p => p.IdColor == request.IdColor)
+                                      .SingleOrDefaultAsync();
+
+            return color;
+        }
+    }
+}
