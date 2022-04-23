7e4b7ff328973e421a91b3cd66d0b627514a301f
Hubert <golab.mlody@gmail.com>
2022-04-23 14:50:19 +0200

    Add color commands

---
+++ Application/Functions/Color/Commands/CreateColorCommand/CreateColorCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Functions.Color.Commands.CreateColorCommand
+{
+    public class CreateColorCommand : IRequest<CreateColorResponse>
+    {
+        public string Name { get; set; }
+        public int IdLink { get; set; }
+        public bool IsForCover { get; set; }
+    }
+}
---
+++ Application/Functions/Color/Commands/DeleteColorCommand/DeleteColorValidator.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.Color.Commands.DeleteColorCommand
+{
+    public class DeleteColorValidator : AbstractValidator<DeleteColorCommand>
+    {
+        private readonly IApplicationContext _context;
+
+        public DeleteColorValidator(IApplicationContext context)
+        {
+            _context = context;
+
+            RuleFor(p => p).
+                MustAsync(DoesColorExists)
+                .WithMessage("Color with given id does not exist.");
+        }
+
+        private async Task<bool> DoesColorExists(DeleteColorCommand command, CancellationToken cancellationToken)
+        {
+            var color = await _context.Colors
+                                      .Where(p => p.IdColor == command.IdColor)
+                                      .SingleOrDefaultAsync();
+
+            return color != null;
+        }
+    }
+}
