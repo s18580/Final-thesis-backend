076077095e405f387e7c500d2faadb43d557b9d9
Hubert <golab.mlody@gmail.com>
2022-04-23 18:01:36 +0200

    Add price list queries

---
+++ Application/Functions/PriceList/Queries/GetPriceListQuery/GetPriceListQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Functions.PriceList.Queries.GetPriceListQuery
+{
+    public class GetPriceListQuery : IRequest<Domain.Models.DictionaryModels.PriceList>
+    {
+        public int IdPriceList { get; set; }
+    }
+}
