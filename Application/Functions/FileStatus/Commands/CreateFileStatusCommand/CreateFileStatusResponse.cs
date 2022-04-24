778f047d9ad021e915494289acbd9dae5f731990
Hubert <golab.mlody@gmail.com>
2022-04-24 12:57:34 +0200

    File status response fixes

--- Application/Functions/FileStatus/Commands/CreateFileStatusCommand/CreateFileStatusResponse.cs
+++ Application/Functions/FileStatus/Commands/CreateFileStatusCommand/CreateFileStatusResponse.cs
@@ -5,8 +5,12 @@ namespace Application.Functions.FileStatus.Commands.CreateFileStatusCommand
 {
     public class CreateFileStatusResponse : BaseResponse
     {
-        public CreateFileStatusResponse() : base()
-        { }
+        public int Id { get; }
+
+        public CreateFileStatusResponse(int id) : base()
+        {
+            Id = id;
+        }
 
         public CreateFileStatusResponse(ValidationResult validationResult, ResponseStatus status)
             : base(validationResult, status)
