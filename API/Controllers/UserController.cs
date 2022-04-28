88f04c66c799e59baf3868af57d464ffa7a5885b
Hubert <golab.mlody@gmail.com>
2022-04-28 20:22:43 +0200

    Create UserController

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Application.Functions.User;
+using Application.Functions.User.Commands.RegisterUserCommand;
+using Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] AnonymousUserDTO data)
+        {
+            var response = await _mediator.Send(new RegisterUserCommand { anonymousUserData = data });
+            if (response.Success)
+            {
+                return Ok(response.loggedUserData);
+            }
+            else if (response.Status == ResponseStatus.ValidationError)
+            {
+                return UnprocessableEntity(response.Message);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginUser([FromBody] AnonymousUserDTO data)
+        {
+            var response = await _mediator.Send(new RegisterUserCommand { anonymousUserData = data });
+            if (response.Success)
+            {
+                return Ok(response.loggedUserData);
+            }
+            else if (response.Status == ResponseStatus.ValidationError)
+            {
+                return UnprocessableEntity(response.Message);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
