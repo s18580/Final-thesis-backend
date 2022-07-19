7924f68abe3dcba99ee748a3168ab84471fff41b
Hubert <golab.mlody@gmail.com>
2022-07-19 22:04:10 +0200

    Adapt controlers and validators to UI data

---
+++ Application/Functions/PriceList/Queries/GetPriceListListQuery/GetPriceListListQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Functions.PriceList.Queries.GetPriceListListQuery
+{
+    public class GetPriceListListQueryHandler : IRequestHandler<GetPriceListListQuery, List<Domain.Models.DictionaryModels.PriceList>>
+    {
+        private readonly IApplicationContext _context;
+        public GetPriceListListQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Domain.Models.DictionaryModels.PriceList>> Handle(GetPriceListListQuery request, CancellationToken cancellationToken)
+        {
+            var priceLists = await _context.PriceLists.ToListAsync();
+
+            return priceLists;
+        }
+    }
+}
