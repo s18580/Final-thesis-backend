96fadc17488d833b31cc5d64d29fd126a67cb168
s18580 <s18580@pjwstk.edu.pl>
2022-08-31 23:17:41 +0200

    Create master test page and constants file with paths and urls

---
+++ Application.Tests/Setup/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Tests.Setup
+{
+    public class Constants
+    {
+        public static string StartURL = "http://localhost:8080/";
+        public static string ChromeDriverPath = "E:\\1. IT\\2. Projekty\\3. Praca Inżynierska\\PROJEKTY\\Final-thesis-backend\\Application.Tests\\Setup";
+    }
+}
---
+++ Application.Tests/Tests/TestMasterPage.cs
@@ -0,0 +1,31 @@
+﻿using Application.Tests.Setup;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Application.Tests.Tests
+{
+    [TestClass]
+    public class TestMasterPage
+    {
+        public IWebDriver _driver { get; }
+
+        public TestMasterPage()
+        {
+            _driver = new ChromeDriver(Constants.ChromeDriverPath);
+            _driver.Manage().Window.Maximize();
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            _driver.Navigate().GoToUrl(Constants.StartURL);
+        }
+
+        [TestCleanup]
+        public void ClenUp()
+        {
+            _driver.Dispose();
+        }
+    }
+}
