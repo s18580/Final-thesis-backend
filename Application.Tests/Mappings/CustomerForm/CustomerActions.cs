6b77215ae3a01b12d52313b190f9ad1fd1ed02ef
s18580 <s18580@pjwstk.edu.pl>
2022-09-03 01:31:07 +0200

    Add customer mappings

---
+++ Application.Tests/Mappings/CustomerForm/CustomerActions.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using System.Threading;
+
+namespace Application.Tests.Mappings.CustomerForm
+{
+    public class CustomerActions
+    {
+        private static CustomerForm CustomerForm { get; set; }
+        public static void InitPage(IWebDriver driver)
+        {
+            CustomerForm = new CustomerForm(driver);
+        }
+
+        public static void SetCustomerTypeOnCompany()
+        {
+            CustomerForm.CompanyType.Click();
+            Thread.Sleep(500);
+        }
+
+        public static void SetCustomerTypeOnPerson()
+        {
+            CustomerForm.PersonType.Click();
+            Thread.Sleep(500);
+        }
+
+        public static void OpenRepresentativeModal()
+        {
+            CustomerForm.AddRepresentativeButton.Click();
+            Thread.Sleep(500);
+        }
+
+        public static void OpenAddressModal()
+        {
+            CustomerForm.AddAddressButton.Click();
+            Thread.Sleep(500);
+        }
+
+        public static void AddCustomer()
+        {
+            CustomerForm.AddCustomerButton.Click();
+            Thread.Sleep(2000);
+        }
+
+        public static void PopulateCompanyDetails(string name, string nip, string regon, string email, string phone)
+        {
+            CustomerForm.CompanyName.SendKeys(name);
+            CustomerForm.CompanyEmail.SendKeys(email);
+            CustomerForm.CompanyPhone.SendKeys(phone);
+            CustomerForm.CompanyNip.SendKeys(nip);
+            CustomerForm.CompanyRegon.SendKeys(regon);
+        }
+
+        public static void PopulatePersonDetails(string name, string lastName, string email, string phone)
+        {
+            CustomerForm.CustomerName.SendKeys(name);
+            CustomerForm.CustomerLastName.SendKeys(lastName);
+            CustomerForm.CustomerPhone.SendKeys(email);
+            CustomerForm.CustomerEmail.SendKeys(phone);
+        }
+    }
+}
---
+++ Application.Tests/Mappings/CustomerForm/CustomerForm.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+
+namespace Application.Tests.Mappings.CustomerForm
+{
+    public class CustomerForm
+    {
+        public IWebDriver _driver { get; }
+        public IWebElement CompanyType => _driver.FindElement(By.XPath("//span[text()='Firma']"));
+        public IWebElement PersonType => _driver.FindElement(By.XPath("//span[text()='Osoba prywatna']"));
+        public IWebElement CompanyName => _driver.FindElement(By.Id("companyName"));
+        public IWebElement CompanyEmail => _driver.FindElement(By.Id("companyEmail"));
+        public IWebElement CompanyPhone => _driver.FindElement(By.Id("companyPhone"));
+        public IWebElement CompanyNip => _driver.FindElement(By.Id("companyNip"));
+        public IWebElement CompanyRegon => _driver.FindElement(By.Id("companyRegon"));
+        public IWebElement CustomerName => _driver.FindElement(By.Id("customerName"));
+        public IWebElement CustomerLastName => _driver.FindElement(By.Id("customerLastName"));
+        public IWebElement CustomerEmail => _driver.FindElement(By.Id("customerEmail"));
+        public IWebElement CustomerPhone => _driver.FindElement(By.Id("customerPhone"));
+
+        public IWebElement AddAddressButton => _driver.FindElement(By.Id("addAddress"));
+        public IWebElement AddRepresentativeButton => _driver.FindElement(By.Id("addRepresentative"));
+        public IWebElement AddCustomerButton => _driver.FindElement(By.Id("addCustomer"));
+
+        public CustomerForm(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+    }
+}
