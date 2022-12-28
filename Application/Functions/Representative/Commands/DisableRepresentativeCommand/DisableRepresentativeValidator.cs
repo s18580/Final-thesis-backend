ece3db89c461c73f344e45e8e90b1ca36dd479e7
s18580 <s18580@pjwstk.edu.pl>
2022-12-28 17:49:38 +0100

    Add new rule for disable representative validator

--- Application/Functions/Representative/Commands/DisableRepresentativeCommand/DisableRepresentativeValidator.cs
+++ Application/Functions/Representative/Commands/DisableRepresentativeCommand/DisableRepresentativeValidator.cs
@@ -15,6 +15,10 @@ namespace Application.Functions.Representative.Commands.DisableRepresentativeCom
             RuleFor(p => p).
                 MustAsync(DoesRepresentativeExists)
                 .WithMessage("Representative with given id does not exist.");
+
+            RuleFor(p => p).
+                MustAsync(CanBeDisabled)
+                .WithMessage("You cannot disable main representative of person client.");
         }
 
         private async Task<bool> DoesRepresentativeExists(DisableRepresentativeCommand command, CancellationToken cancellationToken)
@@ -24,6 +28,18 @@ namespace Application.Functions.Representative.Commands.DisableRepresentativeCom
                                        .SingleOrDefaultAsync();
 
             return representative != null;
+        }
+
+        private async Task<bool> CanBeDisabled(DisableRepresentativeCommand command, CancellationToken cancellationToken)
+        {
+            var representative = await _context.Representatives
+                                       .Include(p => p.Customer)
+                                       .Where(p => p.IdRepresentative == command.Id)
+                                       .SingleOrDefaultAsync();
+
+
+
+            return representative.Name + " " + representative.LastName != representative.Customer.CompanyName;
         }
     }
 }
